b7eb0362ce6ef04f33514def69f0d771e5490e72
kycuzinho <cebolinhas2000@gmail.com>
2025-02-05 19:04:00 +0000

    Visual Changes

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 const HomeBanner = () => {
     return ( 
-        <div className="realtive bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
+        <div className="realtive bg-gradient-to-r from-sky-100 to-sky-700 mb-8 rounded-xl">
             <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
                 <div className="mb-8 md:mb-0 text-center">
-                    <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">Promoção de Primavera</h1>
+                    <h1 className="text-4xl md:text-6xl font-bold bg-gradient-to-r from-blue-600 to-blue-500 bg-clip-text text-transparent mb-4">Promoção de Primavera</h1>
                     <p className="text-lg md:text-xl text-white mb-2">Aproveite os discontos nos items selecionados</p>
                     <p className="text-2xl md:text-5xl text-yellow-400 font-bold">Até 80% de desconto</p>
                 </div>
