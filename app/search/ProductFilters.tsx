5d24c59c226008508c409e1f8d9b6aa993c3b30c
kycuzinho <cebolinhas2000@gmail.com>
2025-04-05 14:21:31 +0100

    `feat: updated ProductFilters component with new icons and layout`

--- app/search/ProductFilters.tsx
+++ app/search/ProductFilters.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback, useState } from "react";
+import { FaFilter } from "react-icons/fa6";
+import { FaFilterCircleXmark } from "react-icons/fa6";
 
 const ProductFilters = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,6 +31,8 @@ const ProductFilters = () => {
     else params.delete("maxPrice");
 
     router.push(`/search?${params.toString()}`);
+
+    setIsOpen(false);
   };
 
   const handleClearFilters = () => {
@@ -45,40 +49,61 @@ const ProductFilters = () => {
     <div className="mb-4">
       <button
         onClick={toggleOpen}
-        className="mb-2 px-4 py-2 bg-sky-600 text-white rounded"
+        className="mb-2 px-4 py-2 bg-sky-600 hover:bg-sky-800 text-white rounded"
       >
-        {isOpen ? "Esconder Filtros" : "Mostrar Filtros"}
+        {isOpen ? <FaFilterCircleXmark /> : <FaFilter />}
       </button>
 
       {isOpen && (
-        <div className="flex flex-wrap gap-4">
-          <input
-            type="number"
-            placeholder="Preço mínimo"
-            value={minPrice}
-            onChange={(e) => setMinPrice(e.target.value)}
-            className="border p-2 rounded w-36"
-          />
-          <input
-            type="number"
-            placeholder="Preço máximo"
-            value={maxPrice}
-            onChange={(e) => setMaxPrice(e.target.value)}
-            className="border p-2 rounded w-36"
-          />
-          <button
-            onClick={handleFilter}
-            className="bg-sky-600 text-white px-4 py-2 rounded"
-          >
-            Filtrar
-          </button>
-          <button
-            onClick={handleClearFilters}
-            className="bg-gray-300 text-black px-4 py-2 rounded"
-          >
-            Limpar Filtros
-          </button>
-        </div>
+        <div className="
+        absolute
+        top-1/2
+        left-1/2
+        -translate-x-1/2
+        -translate-y-1/2
+        w-[90vw]
+        max-w-sm
+        bg-white
+        shadow-md
+        rounded-md
+        p-4
+        z-50
+        shadow-neutral-400
+        flex
+        flex-col
+        gap-4
+      ">
+        <input
+          type="number"
+          placeholder="Preço mínimo"
+          value={minPrice}
+          onChange={(e) => setMinPrice(e.target.value)}
+          className="border p-2 rounded w-full sm:w-40"
+        />
+      
+        <input
+          type="number"
+          placeholder="Preço máximo"
+          value={maxPrice}
+          onChange={(e) => setMaxPrice(e.target.value)}
+          className="border p-2 rounded w-full sm:w-40"
+        />
+      
+        <button
+          onClick={handleFilter}
+          className="bg-sky-600 hover:bg-sky-800 text-white px-4 py-2 rounded w-full"
+        >
+          Filtrar
+        </button>
+        
+        <button
+          onClick={handleClearFilters}
+          className="bg-gray-200 hover:bg-gray-300 text-black px-4 py-2 rounded w-full"
+        >
+          Limpar Filtros
+        </button>
+      </div>
+      
       )}
     </div>
   );
