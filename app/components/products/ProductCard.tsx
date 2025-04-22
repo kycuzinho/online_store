81300aa42bd9bdaab145938e39b53ad0def4b872
kycuzinho <cebolinhas2000@gmail.com>
2025-04-22 20:13:58 +0100

    `feat: updated ProductCard component with wish list functionality`

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,22 +1,52 @@
 'use client';
+import { WishListProdutType } from "@/app/product/[productId]/ProductDetails";
+import { useWishList } from "@/hooks/useWishList";
 import { formatPrice } from "@/utils/formatPrice";
 import { truncateText } from "@/utils/truncateText";
 import { Rating } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { FaBoltLightning } from "react-icons/fa6";
+import { useEffect, useState } from "react";
+import { FaBoltLightning, FaHeart, FaRegHeart } from "react-icons/fa6";
 
 interface ProductCardProp{
+    /* product?: any */
     data: any
     compact?: boolean;
 }
 
-const ProductCard: React.FC<ProductCardProp> = ({data, compact = false}) => {
+const ProductCard: React.FC<ProductCardProp> = ({/* product, */ data, compact = false}) => {
+
+    const {handleAddProductToWishList, handleRemoveProductFromWishList, wishListProducts} = useWishList();
+    const [isProductInWishList, setIsProductInWishList] = useState(false);
 
     const router = useRouter();
 
+    const [wishListProduct, setWishListProduct] = useState<WishListProdutType>({
+        id: data.id,
+        name: data.name,
+        description: data.description,
+        category: data.category,
+        brand: data.brand,
+        selectedImg: {...data.images[0]},
+        price: data.price,
+    });
+
     const productRating = data.reviews.reduce((acc: number, item: any) =>
-        item.rating + acc, 0 ) / data.reviews.length
+        item.rating + acc, 0 ) / data.reviews.length;
+
+
+    useEffect(() => {
+        setIsProductInWishList(false)
+
+        if(wishListProducts){
+            const existingIndex = wishListProducts.findIndex((item) => item.id === data.id) 
+            
+            if(existingIndex > -1){
+                setIsProductInWishList(true);
+            }
+        }
+    }, [data.id, wishListProducts])
 
     return ( 
     <div 
@@ -54,6 +84,26 @@ const ProductCard: React.FC<ProductCardProp> = ({data, compact = false}) => {
                 />
 
             </div>
+            <div
+                className="
+                absolute 
+                top-[10px] 
+                right-[10px] 
+                cursor-pointer 
+                hover:scale-105">
+                    {isProductInWishList 
+                    ? 
+                    <>
+                    <FaHeart size={28} className="text-sky-800" 
+                    onClick={(e) => {e.stopPropagation(); handleRemoveProductFromWishList(wishListProduct)}}/>
+                    </> 
+                    :  
+                    <>
+                    <FaRegHeart size={28} className="text-sky-800" 
+                    onClick={(e) =>{e.stopPropagation(); handleAddProductToWishList(wishListProduct)} }/> 
+                    </> 
+                    }
+                </div>
             <div className="mt-4">
                 {truncateText(data.name)}
             </div>
